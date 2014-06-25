d21a7a4cc9ab8f87276776eebf6395b5278dd54e
Andy Georgia <andygeorgia@hotmail.com>
2014-06-25 17:51:22 -0400

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,168 @@
+'use strict';
+
+Parse.initialize("LFJ3QGbWShkk4EL9OPC9RBzsM1beJbuSzZ3m8F0f", "h9DNGD2Piozeww9Il1F9zS85gQh17uJXZnqf2l7V");
+
+
+var Login = Parse.Object.extend({
+  className: "Login"
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+var LoginCollection = Parse.Collection.extend({
+
+  model: Login,
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+var collection = new LoginCollection();
+
+
+
+var LoginView = Parse.View.extend({
+
+  className: "cool",
+
+  loginTemplate: _.template($('.login-template').text()),
+
+  events: {
+
+    "click .submit": "showClockinModal",
+    "click .yes": "showLayoutView"
+  },
+
+  initialize: function(){
+    $('.login-container').append(this.el);
+    this.render();
+  },
+
+  render: function(){
+    var renderedTemplate = this.loginTemplate();
+    this.$el.html(renderedTemplate);
+  },
+
+  showLayoutView: function(){
+    console.log('button is working')
+    console.log(this.model)
+    new LayoutView();
+    // $('.container').append(layout);
+  },
+
+  showClockinModal: function(){
+    var modal = document.getElementById('clockinoverlay');
+    modal.style.visibility = (modal.style.visibility == "visible") ? "hidden":"visible";
+
+  }
+});
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+var login = new LoginView();
+
+var LayoutView = Parse.View.extend({
+
+  className: "cooler",
+
+  layoutTemplate: _.template($('.layout-template').text()),
+
+  events: {
+
+    "click .hightop1": "showOrderView",
+    "click .hightop2": "showOrderView",
+    "click .table-101": "showOrderView",
+    "click .table-102": "showOrderView",
+    "click .table-103": "showOrderView",
+    "click .table-104": "showOrderView",
+    "click .booth-1": "showOrderView",
+    "click .booth-2": "showOrderView",
+    "click .booth-3": "showOrderView",
+    "click .booth-4": "showOrderView",
+    "click .stool-1": "showOrderView",
+    "click .stool-2": "showOrderView",
+    "click .stool-3": "showOrderView",
+    "click .stool-4": "showOrderView",
+    "click .stool-5": "showOrderView",
+    "click .stool-6": "showOrderView",
+    "click .stool-7": "showOrderView",
+    "click .stool-8": "showOrderView"
+
+  },
+
+  initialize: function(){
+    $('.login-container').empty();
+    $('.layout-container').append(this.el);
+    this.render();
+  },
+
+  render: function(){
+    var renderedTemplate = this.layoutTemplate(this.model);
+    this.$el.html(renderedTemplate);
+  },
+
+
+
+  showOrderView: function(){
+    var order = new OrderView({model: this.model});
+    //$('.container').append(order);
+  }
+
+
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var OrderView = Parse.View.extend({
+
+  className: "coolest",
+
+  orderTemplate: _.template($('.order-template').text()),
+
+  events: {
+
+    "click .go-button": "showLoginView",
+    "click .entrees-button": "showEntreesView",
+    "click .void": "showManagerModal",
+    "click .numberenter": "showOrderView",
+    "click .payment-button": "showPaymentView"
+    
+
+  },
+
+  initialize: function(){
+    $('.layout-container').empty();
+    $('.order-container').append(this.el);
+    this.render();
+  },
+
+  render: function(){
+    var renderedTemplate = this.orderTemplate(this.model);
+    this.$el.html(renderedTemplate);
+  },
+
+  showLoginView: function(){
+    this.remove();
+    var login = new LoginView({model: this.model});
+
+  },
+
+  showEntreesView: function(){
+    var entree = new EntreeView({model: this.model});
+  },
+
+  showManagerModal: function(){
+    var modal = document.getElementById('overlay');
+    modal.style.visibility = (modal.style.visibility == "visible") ? "hidden":"visible";
+  },
+
+  showOrderView: function(){
+    this.remove();
+    var order = new OrderView({model: this.model});
+  },
+
+  showPaymentView: function(){
+    this.remove();
+    var payment = new PaymentView({model: this.model});
+  }
+
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////

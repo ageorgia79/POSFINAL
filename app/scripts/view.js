38c0d4374e87ae048dd89c438b0ccdeb0676f2ed
Andy Georgia <andygeorgia@hotmail.com>
2014-07-16 12:12:00 -0400

    table collection created

--- app/scripts/view.js
+++ app/scripts/view.js
@@ -103,11 +103,24 @@ var LayoutView = Parse.View.extend({
 
   events: {
 
-    "click .hightop1" : "showOrderView",
-    "click .hightop2" : "showOrderView",
-    "click .table"    : "showOrderView",
-    "click .booth"    : "showOrderView",
-    "click .stool"    : "showOrderView",
+    "click .hightop1" : "showHightop1",
+    "click .hightop2" : "showHightop2",
+    "click .table101"    : "showTable101",
+    "click .table102"    : "showTable102",
+    "click .table103"    : "showTable103",
+    "click .table104"    : "showTable104",
+    "click .booth201"    : "showBooth201",
+    "click .booth202"    : "showBooth202",
+    "click .booth203"    : "showBooth203",
+    "click .booth204"    : "showBooth204",
+    "click .stool1"    : "showStool1",
+    "click .stool2"    : "showStool2",
+    "click .stool3"    : "showStool3",
+    "click .stool4"    : "showStool4",
+    "click .stool5"    : "showStool5",
+    "click .stool6"    : "showStool6",
+    "click .stool7"    : "showStool7",
+    "click .stool8"    : "showStool8",
     
 
   },
@@ -124,12 +137,259 @@ var LayoutView = Parse.View.extend({
     this.$el.html(renderedTemplate);
   },
 
-  showOrderView: function(){
- 
+  showHightop1: function(){
+    var table = new Parse.Object('Table');
+    var hightop1 = $('.hightop1').text();
+   
+
+    table.set("hightop1", hightop1);
+   
+
+    table.save();
 
 
     router.navigate("#categories", {trigger: true});
   },
+
+  showHightop2: function(){
+    var table = new Parse.Object('Table');
+    var hightop2 = $('.hightop2').text();
+   
+
+    table.set("hightop2", hightop2);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+  showTable101: function(){
+    var table = new Parse.Object('Table');
+    var table101 = $('.table101').text();
+   
+
+    table.set("table101", table101);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+   showTable102: function(){
+    var table = new Parse.Object('Table');
+    var table102 = $('.table102').text();
+   
+
+    table.set("table102", table102);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+   showTable103: function(){
+    var table = new Parse.Object('Table');
+    var table103 = $('.table103').text();
+   
+
+    table.set("table103", table103);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+   showTable104: function(){
+    var table = new Parse.Object('Table');
+    var table104 = $('.table104').text();
+   
+
+    table.set("table104", table104);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+  showBooth201: function(){
+    var table = new Parse.Object('Table');
+    var booth201 = $('.booth201').text();
+   
+
+    table.set("booth201", booth201);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+   showBooth202: function(){
+    var table = new Parse.Object('Table');
+    var booth202 = $('.booth202').text();
+   
+
+    table.set("booth202", booth202);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+   showBooth203: function(){
+    var table = new Parse.Object('Table');
+    var booth203 = $('.booth203').text();
+   
+
+    table.set("booth203", booth203);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+   showBooth204: function(){
+    var table = new Parse.Object('Table');
+    var booth204 = $('.booth204').text();
+   
+
+    table.set("booth204", booth204);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+   showStool1: function(){
+    var table = new Parse.Object('Table');
+    var stool1 = $('.stool1').text();
+   
+
+    table.set("stool1", stool1);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+    showStool2: function(){
+    var table = new Parse.Object('Table');
+    var stool2 = $('.stool2').text();
+   
+
+    table.set("stool2", stool2);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+    showStool3: function(){
+    var table = new Parse.Object('Table');
+    var stool3 = $('.stool3').text();
+   
+
+    table.set("stool3", stool3);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+    showStool4: function(){
+    var table = new Parse.Object('Table');
+    var stool4 = $('.stool4').text();
+   
+
+    table.set("stool4", stool4);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+    showStool5: function(){
+    var table = new Parse.Object('Table');
+    var stool5 = $('.stool5').text();
+   
+
+    table.set("stool5", stool5);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+    showStool6: function(){
+    var table = new Parse.Object('Table');
+    var stool6 = $('.stool6').text();
+   
+
+    table.set("stool6", stool6);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+    showStool7: function(){
+    var table = new Parse.Object('Table');
+    var stool7 = $('.stool7').text();
+   
+
+    table.set("stool7", stool7);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+    showStool8: function(){
+    var table = new Parse.Object('Table');
+    var stool8 = $('.stool8').text();
+   
+
+    table.set("stool8", stool8);
+   
+
+    table.save();
+
+
+    router.navigate("#categories", {trigger: true});
+  },
+
+
 
 });
     
@@ -269,7 +529,7 @@ var EntreeView = Parse.View.extend({
 
 
     order.save().done(function(){
-      
+
     $('.menu-buttons').html('')
    router.navigate("#categories", {trigger: true});
     })
